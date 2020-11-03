c4a55bfd7f4b06fca49d487682e011b2ac0bcfe6
Comrade-Ivan <1335308@gmail.com>
2020-11-03 16:03:25 +0300

    03/11/2020

---
+++ jslessons/variables.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Переменные и типы данных JS</title>
+</head>
+<body>
+  <h1>Переменные и типы данных</h1>
+  <script> 
+    //alert("Привет мир!");
+    //Перед закрывающимся тэгом body
+    //Однострочный комментарий
+    /*Многострочный комментарий*/
+  </script>
+  <script src="variables.js"></script>
+</body>
+</html>
