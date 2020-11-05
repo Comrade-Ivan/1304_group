90e2703a4f08613284b4c971f2026021d25fdd32
Comrade-Ivan <1335308@gmail.com>
2020-11-05 11:44:41 +0300

    05/11/2020

---
+++ jslessons/ifandfor.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Условия и циклы</title>
+</head>
+<body>
+  <h1>Условия и циклы</h1>
+  <script src="ifandfor.js"></script>
+</body>
+</html>
